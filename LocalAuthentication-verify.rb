c8680f21091fb0cf147a710d17361ae18ce57973
Ezequiel Aceto <eaceto@users.noreply.github.com>
2021-03-05 10:46:37 -0300

    Update LocalAuthentication-verify.rb

--- LocalAuthentication-verify.rb
+++ LocalAuthentication-verify.rb
@@ -3,7 +3,8 @@ class LocalauthenticationVerify < Formula
   homepage "https://github.com/eaceto/LocalAuthentication-Verify"
   sha256 "c13cab1c16fc9afe2c98e401ed7300762a57e48d7f84595c29c72cfac339156c"
   url "https://bintray.com/eaceto/macOS-tools/download_file?file_path=LocalAuthentication-verify"
-
+  version "0.0.1"
+  
   bottle :unneeded
 
   def install
